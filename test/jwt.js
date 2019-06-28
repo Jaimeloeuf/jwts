a1751b0d98680a961543c5a3a04c50d17eec38f1
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-06-28 14:46:27 +0800

    Cleaned up test directory and wrote basic tests: 1.  Deleted the un used jwts-test module.
    2.  Re wrote the jwt.js unit test file to include the bare minimum basic
        tests like import test and all.

--- test/jwt.js
+++ test/jwt.js
@@ -4,13 +4,28 @@
     Unit test for the jwt module.
 */
 
-const jwt = require('../jwt');
+const assert = require('assert');
 
-/*
-    Everytime you want to use a auto gen key pair that is injected into the functions,
+
+describe('Import test', function () {
+    it('jwts module', () => assert(require('../src/index'), "Failed to import 'jwts' module"));
+    it('Crypto, used for automatic key generation', () => assert(require("crypto"), "Failed to import 'crypto' module"));
+});
+
+
+describe('Normal jwts usage', function () {
+    const jwts = require('../src/index');
+
+    it('Jwts create token', () => assert(jwts.create_token(), 'Create token method failed'));
+});
+
+
+/*  - Everytime you want to use a auto gen key pair that is injected into the functions,
     call the apply_keys functions and destructure the stuff out.
-
-    If you just want to use verify token with a key you have, then skip the above shit.
+    - If you just want to use verify token with a key you have, then skip the above shit.
 */
+describe('jwts with apply_keys() usage', function () {
+    const jwts = require('../src/index').apply_keys();
 
-const jwt = require('./jwt').apply_keys();
+    it('Public Key generation', () => assert(jwts.getPublicKey(), "Public Key generation failed"));
+});
